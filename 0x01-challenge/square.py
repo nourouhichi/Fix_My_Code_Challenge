a9c82b174036bb3cedb5af8ff1ab640e801114b3
nourouhichi <1417@holbertonschool.com>
2021-02-21 22:46:00 +0100

    new add

---
+++ 0x01-challenge/square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" square.py
+    square class
+"""
+
+
+class Square():
+    """ Square class """
+
+    width = 0
+    height = 0
+
+    def __init__(self, *args, **kwargs):
+        """ Constructor """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+    def area_of_my_square(self):
+        """ Area of the square """
+        return self.width * self.width
+
+    def permiter_of_my_square(self):
+        """ permiter of my square """
+        return (self.width * 2) + (self.height * 2)
+
+    def __str__(self):
+        """ String representation of the object """
+        return "{}/{}".format(self.width, self.height)
+
+
+if __name__ == "__main__":
+
+    s = Square(width=12, height=9)
+    print(s)
+    print(s.area_of_my_square())
+    print(s.permiter_of_my_square())
